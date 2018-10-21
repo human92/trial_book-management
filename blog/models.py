1d7f431179503cc7b93ae24d0331b5abc0ca1e30
kai <kth6rtuo8@gmail.com>
2018-10-21 14:59:01 +0900

    implement access log counter

--- blog/models.py
+++ blog/models.py
@@ -12,15 +12,15 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.PROTECT)
     title = models.CharField(max_length=200)
     category = models.ForeignKey(BookCategory, on_delete=models.PROTECT)
-    views_number = models.IntegerField(
-            blank = True, null=True)
+    views_number = models.PositiveIntegerField(
+            default=0, blank=True, null=True)
     text = models.TextField()
     created_date = models.DateTimeField(
              default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
     last_updated_date = models.DateTimeField(
-            auto_now = True, blank=True, null=True)
+            auto_now=True, blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
