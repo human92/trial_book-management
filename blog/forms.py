4220cf654afc4057f93b42034080c38dac4fa519
kai <kth6rtuo8@gmail.com>
2018-10-25 00:15:51 +0900

    reflect .gitignore

--- blog/forms.py
+++ blog/forms.py
@@ -2,17 +2,6 @@ from django import forms
 from .models import Post, BookCategory, BookImage
 from datetime import datetime
 
-# EMPTY_CHOICE = (
-#     ('', '-'*10),
-# )
-
-# CATEGORY_CHOICE = (
-#     ('bungaku', '文学・評論'),
-#     ('business', 'ビジネス・経済'),
-#     ('nonfiction', 'ノンフィクション'),
-#     ('science', 'サイエンス・テクノロジー')
-# )
-
 class PostForm(forms.ModelForm):
     title = forms.TextInput
     text = forms.Textarea
