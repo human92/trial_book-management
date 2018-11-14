ec51bf0e2dfa54be988ad08cb811487e24983cdd
kai <kth6rtuo8@gmail.com>
2018-11-14 22:23:48 +0900

    Display title data from django post

--- front_vue/src/components/book_post.vue
+++ front_vue/src/components/book_post.vue
@@ -1,10 +1,15 @@
 <template>
-    <h2>
-        Hello, World
-            <p>
-                {{ posts }}
-            </p>
-    </h2>
+    <v-app>
+        <h1>Test REST API
+            <ul id='post_list'>
+                <li v-for="post in posts" :key="post.title">
+                    {{post.title}}
+                    {{post.category}}
+                    {{post.createdDate}}
+                </li>
+            </ul>
+        </h1>
+    </v-app>
 </template>
 
 <script>
@@ -14,13 +19,18 @@ export default {
     name: 'BookIndex',
     data(){
         return{
-            posts: null
+            posts: []
         }
     },
     mounted (){
         axios
             .get('http://localhost:8000/api/posts/')
             .then(response => (this.posts = response.data))
+            .catch(error => {
+                console.log(error)
+                this.errored = true
+            })
+            .finally(() => this.loading = false)
     },
 }
 </script>
